b607d23db6482913daf7309b8c88e7912b9e8f28
Navaneetha Kannan <navaneetha.k.kannan@oracle.com>
2018-12-29 19:13:35 +0530

    After including setup file

--- calculator.py
+++ calculator.py
@@ -3,9 +3,30 @@ Calculator library containing basic math operations.
 """
 
 
+class sample:
+    pass
+
+
+class Sample2:
+    def __init__(self, a):
+        self.a= a
+
+    def method1(self):
+        """
+        This is a long line to generate error sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+        :return:
+        """
+        pass
+
+    def geta(self):
+        a=10
+        return self.a
+
+
+
 def add(first_term, second_term):
     return first_term + second_term
 
 
-def subtract(first_term, second_term):
+def subtract (first_term, second_term):
     return first_term - second_term
