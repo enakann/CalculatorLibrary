2150c23d5eff052f7cb88ff7213a9a5f8775e749
Navaneetha Kannan <navaneetha.k.kannan@oracle.com>
2018-12-31 13:03:39 +0530

    testing for jenkins

--- calculator.py
+++ calculator.py
@@ -30,3 +30,7 @@ def add(first_term, second_term):
 
 def subtract (first_term, second_term):
     return first_term - second_term
+
+
+if __name__ == '__main__':
+    print("cacluator is  running")
