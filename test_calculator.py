a2440ad613e7fafcced1cab49f140adb83e72459
Navaneetha Kannan <navaneetha.k.kannan@oracle.com>
2019-01-02 20:05:02 +0530

    multiple changes done

--- test_calculator.py
+++ test_calculator.py
@@ -2,14 +2,14 @@
 Unit tests for the calculator library
 """
 
-from .calculator import add, subtract
+import calculator
 
 
 class TestCalculator:
 
     def test_addition(self):
-        assert add(2, 2) == 4
+        assert calculator.add(2, 2) == 4
 
     def test_subtraction(self):
-        assert subtract(4,2) == 2
+        assert calculator.subtract(4,2) == 2
 
